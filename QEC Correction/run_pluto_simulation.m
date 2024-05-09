284a419776429b40cb305f1ec0a71c259c8e8f14
Anishansu Pradhan <anishansu100@gmail.com>
2024-05-08 23:17:02 -0400

    reaarrangement of the files

--- QEC Correction/run_pluto_simulation.m
+++ QEC Correction/run_pluto_simulation.m
@@ -1,17 +1,11 @@
 function run_pluto_simulation()
-    Fs = 30.72e6; % Sampling frequency (e.g., 100 kHz
-    f_tx = 2.005e9; % Transmission frequency 
-    f_rx = 2e9; % Reception frequency 
-    g_obs = 1;
-    theta_obs = 0;
     % set to 1 for simulation
     if(1)
        [y_n, gq, theta_q] = correctIQ_imbalance_t("dummy.csv", 0, 1);
     else
-        [y_n, gq, theta_q] = correctIQ_imbalance_t("dummy.csv", 1, 0);
-        [corrected_signal] = correctIQImbalance(real(y_n), imag(y_n));
+       [y_n, gq, theta_q] = correctIQ_imbalance_t("dummy.csv", 1, 0);
     end
-   %% Trial of  RX qec
+   %% Trial of  RX qec correction
    % [corrected_signal] = correctIQImbalance(real(y_n), imag(y_n));
    % y_i = corrected_signal(:, 1);
    % y_q = corrected_signal(:, 2);
